4a3f785b3775a689ee843a6110ce2301772aad95
Avi Charlop <avicharlop@gmail.com>
2020-12-04 16:19:42 +0200

    add slack card layout

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,11 +1,57 @@
 const fetch = require('node-fetch')
 
-const URL = 'https://raw.githubusercontent.com/acharlop/whocards/main/static/questions.json'
+const BASE_URL = 'https://raw.githubusercontent.com/acharlop/whocards/main'
+const QUESTIONS_URI = '/static/questions.json'
+const IMAGE_URI = '/images/'
+const IMAGE_COUNT = 6
 
-module.exports.getCard = async (event, context, callback) => {
-	const cards = await fetch(URL).then(res => res.json())
+const randomNumber = (max) => Math.floor(Math.random() * max)
+
+const fetchCard = async () => {
+	const cards = await fetch(BASE_URL + QUESTIONS_URI).then(res => res.json())
 	
-	const card = cards[Math.floor(Math.random() * cards.length)].en
+	return cards[randomNumber(cards.length)].en
+}
+
+const getText = async (event, context, callback) => {
+	const card = await fetchCard()
 	
 	callback(null, { statusCode: 200, body: card })
+}
+
+const getCard = async (event, context, callback) => {
+	const card = await fetchCard()
+	const image_url = BASE_URL + IMAGE_URI + randomNumber(IMAGE_COUNT) + '.png'
+	
+	const body = JSON.stringify({
+		blocks: [
+			{
+				type: 'header',
+				text: {
+					type: 'plain_text',
+					text: 'Who Card',
+					emoji: true
+				}
+			},
+			{
+				type: 'section',
+				text: {
+					type: 'mrkdwn',
+					text: `${card.slice(0, -1)} :question:`
+				},
+				accessory: {
+					type: 'image',
+					image_url,
+					alt_text: 'Shroom'
+				}
+			}
+		]
+	})
+	
+	callback(null, { statusCode: 200, body })
+}
+
+module.exports = {
+	getCard,
+	getText,
 }
