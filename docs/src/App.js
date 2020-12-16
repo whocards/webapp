22f346077f042ae59fd159e6f27aa4441c7d5e3f
Avi Charlop <avicharlop@gmail.com>
2020-12-16 13:43:05 +0200

    coming soon

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
+const IMG_NN = 'NN'
+const IMG_URL = `https://github.com/acharlop/whocards/blob/main/images/${IMG_NN}.png?raw=true`
+
+const randomNumber = () => Math.ceil(Math.random() * 6)
+
 function App() {
+  const [img,setImg] = useState(1)
+
+  useEffect(() => {
+    // set interval
+    const interval = setInterval(() => {
+      setImg(randomNumber())
+    }, 3000)
+    // clear interval
+    return () => clearInterval(interval)
+  }, [img])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>Who Cards</h1>
+        <img src={IMG_URL.replace(IMG_NN, img.toString())} className="App-logo" alt="logo" />
+        <p className="coming-soon">
+          Coming soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
