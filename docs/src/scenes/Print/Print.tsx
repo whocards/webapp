efa58f2c555e56166eee6d4d1af05b0eb050d4ec
Avi Charlop <avicharlop@gmail.com>
2021-03-10 08:26:32 -0600

    remove Website scene and move download buttons to column

--- docs/src/scenes/Print/Print.tsx
+++ docs/src/scenes/Print/Print.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet-async';
 import './Print.css'
-import LANGUAGES from '../../data/languages.json';
+import LANGUAGES from 'data/languages.json';
 
 interface PrintProps {}
 
