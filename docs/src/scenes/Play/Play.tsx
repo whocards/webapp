1b3ac635b56d52bd11410b56a6ce2e5b14be9f6d
Avi Charlop <avicharlop@gmail.com>
2021-03-18 10:01:39 -0500

    add shroom to background

--- docs/src/scenes/Play/Play.tsx
+++ docs/src/scenes/Play/Play.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './Play.css';
 import { Questions } from 'components/Questions';
 import { Helmet } from 'react-helmet-async';
+import MyceliumNetwork from 'images/shroom.png'
 
 interface PlayProps {}
 
 export const Play: React.FunctionComponent<PlayProps> = () => {
 
 	return (
-		<div className='flex-center'>
-				<Helmet>
-					<title>Who Cards - Play</title>
-				</Helmet>
+		<div className='play-container flex-center'>
+			<Helmet>
+				<title>Who Cards - Play</title>
+			</Helmet>
+			<img src={MyceliumNetwork} className='mycelium-network' alt='Two mushrooms representing the mycelium network' />
 			<Questions />
 		</div>
 	)
